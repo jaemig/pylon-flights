42ccce8b2e43090d3e26aa4e15402daf23302087
Jan-Emig <jan.emig@pm.me>
2024-10-16 15:08:56 +0200

    feat: Set up drizzle with d1

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { getContext } from '@getcronit/pylon';
+import { drizzle } from 'drizzle-orm/d1';
+
+import * as schema from './schema';
+
+const getDb = () => {
+    const ctx = getContext()
+
+    return drizzle(ctx.env.DB, { schema })
+}
+
+export default getDb;
