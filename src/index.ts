23224d37064728efbcc14d34f3d250b5b2e88355
Jan-Emig <jan.emig@pm.me>
2024-10-19 17:07:15 +0200

    feat: Improve human logic
    - Catches error cases
    - Adds update function

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,38 @@ import 'dotenv/config';
 
 import { app } from '@getcronit/pylon'
 
-import getDb from './db';
+import { addHuman, deleteHuman, getHumanById, getHumans, updateHuman } from './lib/humans';
+import { addAircraft, deleteAircraft, getAircraftById, getAircrafts } from './lib/aircrafts';
 // import seed from './db/seed';
 
 export const graphql = {
   Query: {
-    humans: async () => {
-      return await getDb().query.humans.findMany()
-    },
-    aircrafts: async () => {
-      return await getDb().query.aircrafts.findMany()
-    },
-    airports: async () => {
-      return await getDb().query.airports.findMany()
-    },
-    flights: async () => {
-      return await getDb().query.flights.findMany()
-    },
-    airlines: async () => {
-      return await getDb().query.airlines.findMany()
-    },
-    passengers: async () => {
-      return await getDb().query.passengers.findMany()
-    }
+    humans: getHumans,
+    human: getHumanById,
+    aircrafts: getAircrafts,
+    aircraft: getAircraftById,
+    // aircrafts: async () => {
+    //   return await getDb().query.aircrafts.findMany()
+    // },
+    // airports: async () => {
+    //   return await getDb().query.airports.findMany()
+    // },
+    // flights: async () => {
+    //   return await getDb().query.flights.findMany()
+    // },
+    // airlines: async () => {
+    //   return await getDb().query.airlines.findMany()
+    // },
+    // passengers: async () => {
+    //   return await getDb().query.passengers.findMany()
+    // }
   },
   Mutation: {
+    addHuman,
+    updateHuman,
+    deleteHuman,
+    addAircraft,
+    deleteAircraft,
     // seed: seed,
   }
 }
--- src/lib/humans.ts
+++ src/lib/humans.ts
@@ -1,8 +1,8 @@
 import { and, eq, like, SQL } from "drizzle-orm";
+import { ServiceError } from "@getcronit/pylon";
 
 import getDb from "../db";
 import { humans } from "../db/schema";
-import { ServiceError } from "@getcronit/pylon";
 
 /**
  * Get humans
@@ -28,11 +28,18 @@ export async function getHumans(firstname?: string, lastname?: string, birthdate
     if (lastname) conditions.push(like(humans.lastname, `${lastname.trim().toLocaleLowerCase()}%`));
     if (birthdate) conditions.push(like(humans.birthdate, `${birthdate.trim().toLocaleLowerCase()}%`));
 
-    return await getDb().query.humans.findMany({
-        limit: take ?? 20,
-        offset: skip,
-        where: conditions.length > 0 ? and(...conditions) : undefined, // Use `and` only if there are conditions
-    });
+    try {
+        return await getDb().query.humans.findMany({
+            limit: take ?? 20,
+            offset: skip,
+            where: conditions.length > 0 ? and(...conditions) : undefined, // Use `and` only if there are conditions
+        });
+    } catch {
+        throw new ServiceError('Failed to get humans', {
+            statusCode: 400,
+            code: 'db_error',
+        });
+    }
 }
 
 /**
@@ -47,7 +54,10 @@ export async function getHumanById(id: number) {
                 where: eq(humans.id, id)
             });
     } catch {
-        return undefined;
+        throw new ServiceError('Failed to get human', {
+            statusCode: 400,
+            code: 'db_error',
+        });
     }
 }
 
@@ -59,19 +69,100 @@ export async function getHumanById(id: number) {
  * @returns             The added human
  */
 export async function addHuman(firstname: string, lastname: string, birthdate: string) {
+    const birthdateParts = birthdate.split('-');
+    if (birthdateParts.length !== 3 || birthdateParts[0].length !== 4 || birthdateParts[1].length !== 2 || birthdateParts[2].length !== 2) {
+        throw new ServiceError('Invalid birthdate', {
+            statusCode: 400,
+            code: 'invalid_birthdate',
+            details: {
+                birthdate,
+                format: 'YYYY-MM-DD'
+            }
+        });
+    }
+
+    const firstnameInput = firstname.trim();
+    if (firstnameInput.length === 0) {
+        throw new ServiceError('First name is required', {
+            statusCode: 400,
+            code: 'missing_firstname',
+        });
+    }
+
+    const lastnameInput = lastname.trim();
+    if (lastnameInput.length === 0) {
+        throw new ServiceError('Last name is required', {
+            statusCode: 400,
+            code: 'missing_lastname',
+        });
+    }
+
+
     try {
         const [human] = await getDb()
             .insert(humans)
             .values({
+                firstname: firstnameInput,
+                lastname: lastnameInput,
+                birthdate
+            })
+            .returning();
+        return human;
+    } catch {
+        throw new ServiceError('Failed to add human', {
+            statusCode: 400,
+            code: 'db_error',
+        });
+    }
+
+}
+
+export async function updateHuman(id: number, firstname?: string, lastname?: string, birthdate?: string) {
+    const existingHuman = await getHumanById(id);
+
+    if (!existingHuman) {
+        throw new ServiceError('Human does not exist', {
+            statusCode: 400,
+            code: 'human_does_not_exist',
+            details: {
+                id
+            }
+        });
+    }
+
+    if (birthdate) {
+        const birthdateParts = birthdate.split('-');
+
+        if (birthdateParts.length !== 3 || birthdateParts[0].length !== 4 || birthdateParts[1].length !== 2 || birthdateParts[2].length !== 2) {
+            throw new ServiceError('Invalid birthdate', {
+                statusCode: 400,
+                code: 'invalid_birthdate',
+                details: {
+                    birthdate,
+                    format: 'YYYY-MM-DD'
+                }
+            });
+        }
+
+    }
+
+    try {
+        const [human] = await getDb()
+            .update(humans)
+            .set({
                 firstname,
                 lastname,
                 birthdate
             })
+            .where(eq(humans.id, id))
             .returning();
 
         return human;
     } catch {
-        return undefined;
+        throw new ServiceError('Failed to update human', {
+            statusCode: 400,
+            code: 'db_error',
+        });
     }
 }
 
@@ -93,11 +184,17 @@ export async function deleteHuman(id: number) {
             }
         });
     }
+    try {
+        const [human] = await getDb()
+            .delete(humans)
+            .where(eq(humans.id, id))
+            .returning();
 
-    const [human] = await getDb()
-        .delete(humans)
-        .where(eq(humans.id, id))
-        .returning();
-
-    return human;
+        return human;
+    } catch {
+        throw new ServiceError('Failed to delete human', {
+            statusCode: 400,
+            code: 'db_error',
+        });
+    }
 }
