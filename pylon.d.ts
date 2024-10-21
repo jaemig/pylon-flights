fff03005b041aa9ca0404e3c22124c2c8533592c
Jan-Emig <jan.emig@pm.me>
2024-10-21 19:01:09 +0200

    chore: Add an edit secret variable
    To prevent misuse of the api, any manipulation of the dataset will require a secret to authorize the operation

--- pylon.d.ts
+++ pylon.d.ts
@@ -4,6 +4,7 @@ declare module '@getcronit/pylon' {
   interface Bindings {
     DB: D1Database,
     SEED_SECRET: string,
+    EDIT_SECRET: string,
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
